113f24052c0852069cbce84b9326e3c621337df8
e-ngendo <estherngendo9548@gmail.com>
2023-07-23 03:46:35 -0700

    We are all set

--- _printf.c
+++ _printf.c
@@ -59,7 +59,7 @@ int _printf(const char *format, ...)
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+	write(1, &buffer[0], *buff_ind);
 
-		*buff_ind = 0;
+	*buff_ind = 0;
 }
